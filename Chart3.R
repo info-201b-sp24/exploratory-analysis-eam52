56e80b9d984542e7e8ce32930c3e5aa9e5d9e5f1
angelasinuosun <sinuos2@uw.edu>
2024-05-16 19:56:32 -0700

    Complete Chart3

--- Chart3.R
+++ Chart3.R
@@ -1,1 +1,28 @@
+# Author: Angela Sun
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
 
+raw_data <- read.csv("Survey on Student Mental Health1.csv")
+data <- raw_data %>%
+  mutate(department = trimws(Department), Feeling_during_Pandemic = 
+           str_extract(How_did_you_felt_during_COVID_19_pandemic_period, "^[^,]*"))
+
+data_influence <- data %>%
+  mutate(Mental_Impacts_of_COVID = if_else(Feeling_during_Pandemic == "Happy", "Positive", "Negative"))
+top_stress<- data_influence %>%
+  count(From_where_you_are_getting_stress) %>%
+  arrange(desc(n)) %>%
+  slice_head(n = 4)
+data_influence <- data_influence %>%
+  filter(From_where_you_are_getting_stress %in% top_stress$From_where_you_are_getting_stress & Cgpa > 5)
+
+chart3 <- ggplot(data_influence, aes(x = factor(Mental_Impacts_of_COVID), 
+                                     y = Cgpa,
+                                     fill= From_where_you_are_getting_stress)) + 
+  geom_boxplot() +
+  labs(title = "Impact of the Pandemic on Students' Mental and Academic Well-being", 
+       x = "Positive or Negative Mental Impacts of COVID-19", y = "GPA") +
+  theme(axis.text.x = element_text(size = 11)) +
+  scale_fill_discrete(name = "Worries")
+chart3
