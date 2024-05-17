ecc0106a43d24c34e11cd184062505e227902790
angelasinuosun <sinuos2@uw.edu>
2024-05-16 19:44:19 -0700

    Complete Chart2

--- Chart2.R
+++ Chart2.R
@@ -1,1 +1,22 @@
+# Author: Angela Sun
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
 
+data <- raw_data %>%
+  mutate(department = trimws(Department), Feeling_during_Pandemic = 
+           str_extract(How_did_you_felt_during_COVID_19_pandemic_period, "^[^,]*"))
+top_10_department <- data %>%
+  count(department) %>%
+  arrange(desc(n)) %>%
+  slice_head(n = 10)
+data_department <- data %>%
+  filter(department %in% top_10_department$department)
+
+chart2 <- ggplot(data_department, aes(x = When_you_are_stressed_more, fill = department)) + 
+  geom_bar(stat = "count") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(title = "Relationship between Major and Source of Stress",
+       x = "During Heightened Stress", y = "Number of Students") +
+  scale_fill_discrete(name = "Major")
+chart2
