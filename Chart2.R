29d86c9f9ebb8c593010b2fb9703c7f33734dfac
angelasinuosun <sinuos2@uw.edu>
2024-05-16 20:33:27 -0700

    Add code comments to Chart2

--- Chart2.R
+++ Chart2.R
@@ -3,16 +3,21 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
+# Clean the Data
+raw_data <- read.csv("Survey on Student Mental Health1.csv")
 data <- raw_data %>%
   mutate(department = trimws(Department), Feeling_during_Pandemic = 
            str_extract(How_did_you_felt_during_COVID_19_pandemic_period, "^[^,]*"))
+# Identify the ten departments with the highest student enrollment
 top_10_department <- data %>%
   count(department) %>%
   arrange(desc(n)) %>%
   slice_head(n = 10)
 data_department <- data %>%
   filter(department %in% top_10_department$department)
-
+# Create Chart2:
+# A stacked bar chart designed to answer research question 2:
+# Do students who are studying a certain major report higher stress issues/levels?
 chart2 <- ggplot(data_department, aes(x = When_you_are_stressed_more, fill = department)) + 
   geom_bar(stat = "count") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
