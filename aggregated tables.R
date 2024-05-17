11dd958ce2aba485040f31d9cf6c7159dace5a8f
Ekram <128111692+eam52@users.noreply.github.com>
2024-05-17 15:43:14 -0700

    Add files via upload

---
+++ aggregated tables.R
@@ -0,0 +1,27 @@
+library("dplyr")
+library("tidyverse")
+
+data_set <- read.csv("~/Documents/exploratory-analysis-eam52/Survey on Student Mental Health1.csv")
+
+mutate_col_names <- data_set %>%
+  mutate(
+    age = Age,
+    grade = Current_year_you_are_studying_in,
+    major = Department,
+    sleep_amount = How_much_time_are_you_sleeping_everyday,
+    gpa = Cgpa,
+    reason = Why_did_you_chosen_your_department_work_area,
+    attitude = Are_you_happy_with_your_department_work_area
+  )
+
+# Group by major and reason for the choosing of the major
+agregated_data_frame <- mutate_col_names %>%
+  group_by(major, reason, attitude, sleep_amount) %>%
+  summarize(
+    average_gpa = mean(gpa, na.rm = TRUE),
+    maximum_gpa = max(gpa, na.rm = TRUE),
+    median_gpa = median(gpa, na.rm = TRUE),
+    total_students = n()
+  )
+
+print(agregated_data_frame, n = nrow(agregated_data_frame))
