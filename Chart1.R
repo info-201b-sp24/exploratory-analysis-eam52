6d28760ac72e4bbc10600717e71208d073b8c94c
angelasinuosun <sinuos2@uw.edu>
2024-05-16 20:32:11 -0700

    Add code comments to Chart1

--- Chart1.R
+++ Chart1.R
@@ -3,12 +3,14 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
+# Clean the Data
 raw_data <- read.csv("Survey on Student Mental Health1.csv")
-
 data <- raw_data %>%
   mutate(department = trimws(Department), Feeling_during_Pandemic = 
            str_extract(How_did_you_felt_during_COVID_19_pandemic_period, "^[^,]*"))
-
+# Create Chart 1
+# A stacked bar chart designed to answer research question 1:
+# Is there a relationship between mental health issues among college students and their dietary habits?
 chart1 <- ggplot(data, aes(x = Feeling_during_Pandemic, 
                            fill = Are_you_getting_good_food_diet_everyday)) + 
   geom_bar(stat = "count") +
